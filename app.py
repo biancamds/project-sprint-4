85ac3dd10a46afa4346292df1cc18d8e1e7e6e01
biancamds <arq.biancamayara@gmail.com>
2024-01-14 00:01:43 -0300

    inclusão de caixa de seleção

--- app.py
+++ app.py
@@ -7,10 +7,38 @@ car_data = pd.read_csv('vehicles.csv')  # lendo os dados
 # título da visualização web
 st.header('Visualização de dados de anúncio de vendas de carros')
 
-hist_button = st.button('Criar histograma')  # criar um botão
+# hist_button = st.button('Criar histograma')  # criar um botão
 
 
-if hist_button:  # se o botão for clicado
+# if hist_button:  # se o botão for clicado
+#     # escrever uma mensagem
+#     st.write(
+#         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+#     # criar um histograma
+#     fig = px.histogram(car_data, x="odometer")
+
+#     # exibir um gráfico Plotly interativo
+#     st.plotly_chart(fig, use_container_width=True)
+
+
+# scatter_button = st.button('Criar gráfico de dispersão')  # criar um botão
+
+# if scatter_button:  # se o botão for clicado
+#     # escrever uma mensagem
+#     st.write(
+#         'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+#     # criar um gráfico de dispersão
+#     fig = px.scatter(car_data, y="odometer", x="model_year")
+
+#     # exibir um gráfico Plotly interativo
+#     st.plotly_chart(fig, use_container_width=True)
+
+# criar um botão
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram:  # se o botão 'build_histogram' for clicado
     # escrever uma mensagem
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
@@ -21,16 +49,16 @@ if hist_button:  # se o botão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
+# criar um botão
+build_scatter = st.checkbox('Criar um gráfico de dispersão')
 
-disp_button = st.button('Criar gráfico de dispersão')  # criar um botão
-
-if disp_button:  # se o botão for clicado
+if build_scatter:  # se o botão 'build_scatter' for clicado
     # escrever uma mensagem
     st.write(
         'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um gráfico de dispersão
-    fig = px.scatter(car_data, y="odometer", x="model_year")
+    fig = px.histogram(car_data, x="odometer")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
